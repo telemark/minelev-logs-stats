a47eea89f5bb1badf464430e324e0e7c11768ba9
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-11 14:14:07 +0200

    Updates dependency. Moves cors. Adds logging

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const marked = require('marked')
 const { send } = require('micro')
 const resolveRequest = require('./lib/resolve-request')
 const handleStats = require('./lib/handle-stats')
+const logger = require('./lib/logger')
 
 module.exports = async (request, response) => {
   const query = await resolveRequest(request)
-
-  if (!query.domain === 'frontpage') {
-    response.setHeader('Access-Control-Allow-Origin', '*')
-  }
   try {
     if (query.domain === 'stats') {
+      logger('info', ['index', 'stats'])
       const result = await handleStats(query)
+      response.setHeader('Access-Control-Allow-Origin', '*')
       send(response, 200, result)
     } else {
+      logger('info', ['index', 'frontpage'])
       const readme = readFileSync('./README.md', 'utf-8')
       const html = marked(readme)
       send(response, 200, html)
     }
   } catch (error) {
-    console.error(error)
+    logger('error', ['index', error])
     send(response, 500, error)
   }
 }
