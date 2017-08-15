628fa1a0615f0b2f555637ea2febdc2af0897b6f
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-08-15 12:16:03 +0200

    Updates

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict'
-process.env.DB = 'mongodb://minelev-system:afcacd42-6689-48a5-b538-29e16952fd5d@aws-eu-central-1-portal.0.dblayer.com:15604,aws-eu-central-1-portal.1.dblayer.com:15604/minelev-logs?ssl=true'
+
 module.exports = {
   DB: process.env.DB || 'mongodb://localhost/louie',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-logs',
