8feffca9cdff621d826f44c1e7dcc48286e4f099
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-03-06 12:40:39 +0100

    Updates engines. Moves out of src (patch)

--- test/routes/frontpage-get-test.js
+++ test/routes/frontpage-get-test.js
@@ -2,7 +2,7 @@ const test = require('ava')
 const listen = require('test-listen')
 const axios = require('axios')
 const micro = require('micro')
-const srv = require('../../src/index')
+const srv = require('../../index')
 
 const getUrl = fn => {
   const srv = micro(fn)
