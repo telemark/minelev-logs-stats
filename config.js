34fcce37a68a5b6290e6e8e8e677b50d3edf699d
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-03-06 12:52:14 +0100

    Moves connection to env (patch)

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  DB: process.env.DB || 'mongodb://localhost/louie',
-  PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-logs',
-  PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
-  PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
-}
